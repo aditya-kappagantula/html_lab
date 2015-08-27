e90531c3261bc8a7f78d5e14e219631f40984339
Aditya Kappagantula <kappagantulaaditya@gmail.com>
2015-08-27 00:48:22 -0700

    added modernizr

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,6 +112,7 @@ module.exports = function(grunt){
 
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-handlebars');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
